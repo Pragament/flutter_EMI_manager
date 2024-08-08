d5440942cde37acf9fda638f13a2a526be0af141
Prajyot Dange <prajyotdange32@gmail.com>
2024-08-08 12:31:31 +0530

    new commits

--- lib/Components/table_and_chart.dart
+++ lib/Components/table_and_chart.dart
@@ -1,6 +1,4 @@
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 
@@ -67,174 +65,149 @@ class _TableAndChartState extends State<TableAndChart> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Amortization schedule"),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Table(
-                    border: TableBorder.all(color: Colors.black),
-                    children: [
-                      const TableRow(children: [
-                        TableCell(
-                            verticalAlignment:
-                                TableCellVerticalAlignment.middle,
-                            child: Center(child: Text("year"))),
-                        TableCell(
-                            verticalAlignment:
-                                TableCellVerticalAlignment.middle,
-                            child: Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Center(child: Text("Yearly EMI")),
-                            )),
-                        TableCell(
-                            verticalAlignment:
-                                TableCellVerticalAlignment.middle,
-                            child: Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Text("Intrest paid yearly"),
-                            )),
-                        TableCell(
-                            verticalAlignment:
-                                TableCellVerticalAlignment.middle,
-                            child: Padding(
-                              padding: EdgeInsets.all(5.0),
-                              child:
-                                  Center(child: Text("principle paid Yearly")),
-                            )),
-                        TableCell(
-                            verticalAlignment:
-                                TableCellVerticalAlignment.middle,
-                            child: Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Center(child: Text("closing balance")),
-                            )),
-                      ]),
-                      TableRow(children: [
-                        TableCell(child: Center(child: Text(date.toString()))),
-                        const TableCell(child: Center(child: Text("-"))),
-                        const TableCell(child: Center(child: Text("-"))),
-                        const TableCell(child: Center(child: Text("-"))),
-                        TableCell(
-                            child: Center(
-                                child:
-                                    Text(principleBalance.toInt().toString()))),
-                      ]),
-                      ...List.generate(widget.tenure.toInt(), (index) {
-                        calculateData();
-                        return openingbalance > 0
-                            ? TableRow(children: [
-                                TableCell(
-                                    child: Center(
-                                        child: Text((date++).toString()))),
-                                TableCell(
-                                    child: Center(
-                                        child: Text(
-                                            indianFormatNumber(yearlyEmi)))),
-                                TableCell(
-                                    child: Center(
-                                        child: Text(
-                                            indianFormatNumber(computedDue)))),
-                                TableCell(
-                                    child: Center(
-                                        child: Text(
-                                            indianFormatNumber(principleDue)))),
-                                TableCell(
-                                    child: Center(
-                                        child: Text(principleBalance
-                                            .toInt()
-                                            .toString()))),
-                              ])
-                            : const TableRow(children: [
-                                TableCell(child: Text("year")),
-                                TableCell(child: Text("")),
-                                TableCell(child: Text("")),
-                                TableCell(child: Text("")),
-                                TableCell(child: Text("")),
-                              ]);
-                      }),
-                    ]),
-              ),
-              data.isNotEmpty
-                  ? Container(
-                      margin: const EdgeInsets.symmetric(vertical: 20),
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      height: 350,
-                      width: double.infinity,
-                      child: BarChart(BarChartData(
-                          maxY: widget.loanAmount,
-                          minY: 0,
-                          borderData: FlBorderData(show: false),
-                          gridData: const FlGridData(show: false),
-                          titlesData: const FlTitlesData(
-                              topTitles: AxisTitles(
-                                  sideTitles: SideTitles(showTitles: false)),
-                              leftTitles: AxisTitles(
-                                  sideTitles: SideTitles(showTitles: false)),
-                              rightTitles: AxisTitles(
-                                  sideTitles: SideTitles(showTitles: false))),
-                          barGroups: List.generate(
-                            widget.tenure.toInt(),
-                            (index) => BarChartGroupData(
-                                x: ((DateTime.now().year + index) - 2000),
-                                barRods: [
-                                  BarChartRodData(
-                                      color: Colors.blue,
-                                      borderRadius: BorderRadius.zero,
-                                      toY: data[index],
-                                      width: 10,
-                                      backDrawRodData:
-                                          BackgroundBarChartRodData(
-                                              toY: widget.loanAmount,
-                                              show: true,
-                                              color: Colors.orange))
-                                ]),
-                          ))))
-                  : Container(),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(children: [
-                    ColoredBox(
-                      color: Colors.orange,
-                      child: SizedBox(
-                        height: 20,
-                        width: 20,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text("Intrest")
-                  ]),
-                  Row(
-                    children: [
-                      ColoredBox(
-                        color: Colors.blue,
-                        child: SizedBox(
-                          height: 20,
-                          width: 20,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("principle")
-                    ],
-                  ),
-                ],
-              ),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Table(border: TableBorder.all(color: Colors.black), children: [
+            const TableRow(children: [
+              TableCell(
+                  verticalAlignment: TableCellVerticalAlignment.middle,
+                  child: Center(child: Text("year"))),
+              TableCell(
+                  verticalAlignment: TableCellVerticalAlignment.middle,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(child: Text("Yearly EMI")),
+                  )),
+              TableCell(
+                  verticalAlignment: TableCellVerticalAlignment.middle,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Intrest paid yearly"),
+                  )),
+              TableCell(
+                  verticalAlignment: TableCellVerticalAlignment.middle,
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Center(child: Text("principle paid Yearly")),
+                  )),
+              TableCell(
+                  verticalAlignment: TableCellVerticalAlignment.middle,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(child: Text("closing balance")),
+                  )),
             ]),
-          ),
+            TableRow(children: [
+              TableCell(child: Center(child: Text(date.toString()))),
+              const TableCell(child: Center(child: Text("-"))),
+              const TableCell(child: Center(child: Text("-"))),
+              const TableCell(child: Center(child: Text("-"))),
+              TableCell(
+                  child:
+                      Center(child: Text(principleBalance.toInt().toString()))),
+            ]),
+            ...List.generate(widget.tenure.toInt(), (index) {
+              calculateData();
+              return openingbalance > 0
+                  ? TableRow(children: [
+                      TableCell(
+                          child: Center(child: Text((date++).toString()))),
+                      TableCell(
+                          child: Center(
+                              child: Text(indianFormatNumber(yearlyEmi)))),
+                      TableCell(
+                          child: Center(
+                              child: Text(indianFormatNumber(computedDue)))),
+                      TableCell(
+                          child: Center(
+                              child: Text(indianFormatNumber(principleDue)))),
+                      TableCell(
+                          child: Center(
+                              child:
+                                  Text(principleBalance.toInt().toString()))),
+                    ])
+                  : const TableRow(children: [
+                      TableCell(child: Text("year")),
+                      TableCell(child: Text("")),
+                      TableCell(child: Text("")),
+                      TableCell(child: Text("")),
+                      TableCell(child: Text("")),
+                    ]);
+            }),
+          ]),
         ),
-      ),
+        data.isNotEmpty
+            ? Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                height: 350,
+                width: double.infinity,
+                child: BarChart(BarChartData(
+                    maxY: widget.loanAmount,
+                    minY: 0,
+                    borderData: FlBorderData(show: false),
+                    gridData: const FlGridData(show: false),
+                    titlesData: const FlTitlesData(
+                        topTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false)),
+                        leftTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false)),
+                        rightTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false))),
+                    barGroups: List.generate(
+                      widget.tenure.toInt(),
+                      (index) => BarChartGroupData(
+                          x: ((DateTime.now().year + index) - 2000),
+                          barRods: [
+                            BarChartRodData(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.zero,
+                                toY: data[index],
+                                width: 10,
+                                backDrawRodData: BackgroundBarChartRodData(
+                                    toY: widget.loanAmount,
+                                    show: true,
+                                    color: Colors.orange))
+                          ]),
+                    ))))
+            : Container(),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(children: [
+              ColoredBox(
+                color: Colors.orange,
+                child: SizedBox(
+                  height: 20,
+                  width: 20,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text("Intrest")
+            ]),
+            Row(
+              children: [
+                ColoredBox(
+                  color: Colors.blue,
+                  child: SizedBox(
+                    height: 20,
+                    width: 20,
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("principle")
+              ],
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -140,7 +140,7 @@ class _HomePageState extends State<HomePage> {
                     "టెలిగు",
                     style: TextStyle(fontSize: 20),
                   )),
-            )
+            ) 
           ]),
         );
       }),
